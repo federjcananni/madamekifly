a4360f2d758a3a675809c56449a3e411626f8eb6
federjcananni <82090961+federjcananni@users.noreply.github.com>
2021-05-11 13:09:47 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -29,6 +29,40 @@
             }
         }
     });
+      //Prevents "pull to reload" behaviour in Chrome. Assign to child scrollable elements.
+angular.module('hereApp.directive').directive('noPullToReload', function() {
+    'use strict';
+
+    return {
+        link: function(scope, element) {
+            var initialY = null,
+                previousY = null,
+                bindScrollEvent = function(e){
+                    previousY = initialY = e.touches[0].clientY;
+
+                    // Pull to reload won't be activated if the element is not initially at scrollTop === 0
+                    if(element[0].scrollTop <= 0){
+                        element.on("touchmove", blockScroll);
+                    }
+                },
+                blockScroll = function(e){
+                    if(previousY && previousY < e.touches[0].clientY){ //Scrolling up
+                        e.preventDefault();
+                    }
+                    else if(initialY >= e.touches[0].clientY){ //Scrolling down
+                        //As soon as you scroll down, there is no risk of pulling to reload
+                        element.off("touchmove", blockScroll);
+                    }
+                    previousY = e.touches[0].clientY;
+                },
+                unbindScrollEvent = function(e){
+                    element.off("touchmove", blockScroll);
+                };
+            element.on("touchstart", bindScrollEvent);
+            element.on("touchend", unbindScrollEvent);
+        }
+    };
+});
 
     // Closes responsive menu when a scroll trigger link is clicked
     $(".js-scroll-trigger").click(function () {
