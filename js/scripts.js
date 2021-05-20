51a169404aff2b912d09a4d0fb71b9307b0e209d
federjcananni <82090961+federjcananni@users.noreply.github.com>
2021-05-20 19:19:53 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,7 @@
                     {
                         scrollTop: target.offset().top - 72,
                     },
-                    1000,
+                    700,
                     "easeInOutExpo"
                 );
                 return false;
